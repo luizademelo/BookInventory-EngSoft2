13e9e472c40613f45e37f45f581419a460fce8b9
Luiza de Melo <ludemelo5452@gmail.com>
2024-06-09 08:56:11 -0300

    update bookinventory.test.js

--- bookInventory.test.js
+++ bookInventory.test.js
@@ -3,8 +3,8 @@ import { Book, BookInventory } from ".";
 
 describe('Book', () => { it('should create a new book object with title, description and year properties', () => { const book = new Book('The Great Gatsby', 'A novel by F. Scott Fitzgerald', 1925); expect(book.title).toEqual('The Great Gatsby'); expect(book.description).toEqual('A novel by F. Scott Fitzgerald'); expect(book.year).toEqual(1925); }); });
 
-describe('BookInventory', () => {
-    let inventory;
+// describe('BookInventory', () => {
+//     let inventory;
 
-    beforeEach(() => { inventory = new BookInventory(); })
-});
+//     beforeEach(() => { inventory = new BookInventory(); })
+// });
