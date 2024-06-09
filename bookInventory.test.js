54882e3db2cb170a929882123ca1a752bf18008c
Luiza de Melo <ludemelo5452@gmail.com>
2024-06-09 09:02:20 -0300

    criação de mais testes

--- bookInventory.test.js
+++ bookInventory.test.js
@@ -1,10 +1,25 @@
 import { Book, BookInventory } from ".";
 
 
-describe('Book', () => { it('should create a new book object with title, description and year properties', () => { const book = new Book('The Great Gatsby', 'A novel by F. Scott Fitzgerald', 1925); expect(book.title).toEqual('The Great Gatsby'); expect(book.description).toEqual('A novel by F. Scott Fitzgerald'); expect(book.year).toEqual(1925); }); });
+describe('Book', () => {
+    it('criacao de um livro', () => {
+        const book = new Book('The Great Gatsby', 'A novel by F. Scott Fitzgerald', 1925);
+        expect(book.title).toEqual('The Great Gatsby');
+        expect(book.description).toEqual('A novel by F. Scott Fitzgerald');
+        expect(book.year).toEqual(1925);
+    });
+});
 
-// describe('BookInventory', () => {
-//     let inventory;
+describe('BookInventory', () => {
+    let inventory;
 
-//     beforeEach(() => { inventory = new BookInventory(); })
-// });
+    beforeEach(() => { inventory = new BookInventory(); })
+
+    it('adicao de um livro', () => {
+        inventory.addBook('The Great Gatsby', 'A novel by F. Scott Fitzgerald', 1925);
+        expect(inventory.getSize()).toBe(1);
+        expect(inventory.getBook('The Great Gatsby')).toEqual(
+            new Book('The Great Gatsby', 'A novel by F. Scott Fitzgerald', 1925)
+        );
+    });
+});
