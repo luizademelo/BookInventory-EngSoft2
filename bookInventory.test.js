ef92463c5561ef283e947d746f94c3ada87a947d
Luiza de Melo <ludemelo5452@gmail.com>
2024-06-09 09:11:06 -0300

    adicao de teste de update

--- bookInventory.test.js
+++ bookInventory.test.js
@@ -25,6 +25,14 @@ describe('BookInventory', () => {
 
     it('delete de um livro', () => {
         inventory.deleteBook('The Great Gatsby');
-        expect(inventory.getSize()).toBe(1);
+        expect(inventory.getSize()).toBe(0);
+    });
+
+    it('update de um livro', () => {
+        inventory.addBook('The Great Gatsby', 'A novel by F. Scott Fitzgerald', 1925);
+        inventory.updateBook('The Great Gatsby', 'A novel by the american F. Scott Fitzgerald', 1925);
+        expect(inventory.getBook('The Great Gatsby')).toEqual(
+            new Book('The Great Gatsby', 'A novel by the american F. Scott Fitzgerald', 1925)
+        );
     })
 });
